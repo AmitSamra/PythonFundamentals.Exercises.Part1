b7c3c58a3f03e07325377ae708e056615d00c5e1
Amit Samra <asamra@zipcodes-MacBook-Pro.local>
2020-03-03 17:27:43 -0500

    Implement Exercise 5

---
+++ full_name.py
@@ -0,0 +1,16 @@
+'''
+Exercise 5.a
+Using Terminal, make a copy of first_name_500_times.py and call it full_name.py
+
+cp first_name_500_times.py full_name.py
+'''
+
+first_name = 'Amit'
+#print(first_name*500)
+
+last_name = 'Samra'
+
+full_name = first_name + ' ' + last_name
+
+print(full_name)
+
